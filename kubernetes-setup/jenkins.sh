f7645eab3bbc4fa8283b821c4a9784ed7a49df02
Okan OKUTKAN <okutkan@gmail.com>
2020-12-30 05:15:15 +0300

    path fixed

--- kubernetes-setup/jenkins.sh
+++ kubernetes-setup/jenkins.sh
@@ -2,4 +2,4 @@
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 #helm install jenkins jenkins/jenkins  -n jenkins
-helm install jenkins -n jenkins -f ./jenkins/jenkins-values.yaml jenkins/jenkins 
+helm install jenkins -n jenkins -f jenkins/jenkins-values.yaml jenkins/jenkins 
