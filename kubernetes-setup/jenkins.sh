f77dacfc5e3cd818efddd128ac8ce589264683e3
Okan OKUTKAN <okutkan@gmail.com>
2020-12-30 03:40:47 +0300

    added services

--- kubernetes-setup/jenkins.sh
+++ kubernetes-setup/jenkins.sh
@@ -1,4 +1,4 @@
 # jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm install jenkins jenkins/jenkins 
+helm install jenkins jenkins/jenkins  -n jenkins
--- kubernetes-setup/nexus.sh
+++ kubernetes-setup/nexus.sh
@@ -4,4 +4,4 @@
 helm repo add stevehipwell https://stevehipwell.github.io/helm-charts/
 helm repo update
 #helm upgrade --install --namespace default --values ./my-values.yaml my-release stevehipwell/nexus3
-helm install sonatype stevehipwell/nexus3
+helm install sonatype stevehipwell/nexus3 -n nexus
