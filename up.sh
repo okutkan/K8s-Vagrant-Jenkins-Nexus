06257276aa1c3743606c95284be7e50899915c20
Okan OKUTKAN <okutkan@gmail.com>
2020-12-30 03:15:51 +0300

    added jenkins role

--- up.sh
+++ up.sh
@@ -1,2 +1,13 @@
 vagrant --version
 vagrant up
+
+# jenkins
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+helm install jenkins jenkins/jenkins 
+
+#nexus
+helm repo add stevehipwell https://stevehipwell.github.io/helm-charts/
+helm repo update
+#helm upgrade --install --namespace default --values ./my-values.yaml my-release stevehipwell/nexus3
+helm install sonatype stevehipwell/nexus3
