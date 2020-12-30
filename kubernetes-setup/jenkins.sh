13a06818dda255cfba784075691ea5c7ffa85f16
Okan OKUTKAN <okutkan@gmail.com>
2020-12-30 05:02:14 +0300

    added jenkins values

--- kubernetes-setup/jenkins.sh
+++ kubernetes-setup/jenkins.sh
@@ -1,4 +1,5 @@
 # jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm install jenkins jenkins/jenkins  -n jenkins
+#helm install jenkins jenkins/jenkins  -n jenkins
+helm install jenkins -n jenkins -f ./jenkins/jenkins-values.yaml jenkins/jenkins 
